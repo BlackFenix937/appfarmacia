2e5a26834276eb11d903b0ffbe0fddcff9427751
andyalc <andygonzalez1cmt18b@gmail.com>
2025-09-11 16:18:07 -0600

    Creación de la ruta "Pais" para la vista de aplicación móvil

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'pais',
+    loadChildren: () => import('./pais/pais.module').then( m => m.PaisPageModule)
   }
 ];
 @NgModule({
