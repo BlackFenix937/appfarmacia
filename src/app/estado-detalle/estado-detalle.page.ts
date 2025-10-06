7da90e0327b70bd723d88c493e529ac5612b9d6d
andyalc <andygonzalez1cmt18b@gmail.com>
2025-10-06 13:34:49 -0600

    Añadidos estado detalle

--- src/app/estado-detalle/estado-detalle.page.ts
+++ src/app/estado-detalle/estado-detalle.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
+import axios from 'axios';
 
 @Component({
   selector: 'app-estado-detalle',
@@ -9,10 +12,36 @@ import { Component, OnInit } from '@angular/core';
 export class EstadoDetallePage implements OnInit {
 
   constructor(
-    
+    private route: ActivatedRoute,
+    private loading: LoadingController
   ) { }
 
-  ngOnInit() {
+  estado:any=false;
+
+  ngOnInit():void {
+    this.cargarEstado();
   }
+
+  async cargarEstado() {
+  const estd_id = this.route.snapshot.paramMap.get('estd_id');
+  const loading = await this.loading.create({
+    message: 'Cargando',
+    spinner: 'bubbles',
+  });
+  await loading.present();
+  const response = await axios({
+    method: 'get',
+    url: "http://localhost:8080/estados/"+estd_id+"?expand=paisNombre",
+    withCredentials: true,
+    headers: {
+      'Accept': 'application/json'
+    }
+  }).then((response) => {
+    this.estado = response.data;
+  }).catch(function (error) {
+    console.log(error);
+  });
+  loading.dismiss();
+}
 
 }
